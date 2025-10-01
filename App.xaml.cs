bbe29ee26617dea3529a40633764e9c9c3712ecb
unknown <mkaczmarczykszkola@gmail.com>
2025-10-01 17:12:06 +0200

    Stan na dzień 2025-10-01

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FormularzStudenta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormularzStudenta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StringBuilder studentsData = new StringBuilder();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Walidacja
+            if (string.IsNullOrEmpty(firstNameTextbox.Text) ||
+    string.IsNullOrEmpty(lastNameTextbox.Text) ||
+    birthDatepicker.SelectedDate == null ||
+   fieldOfStudies.SelectedItem == null) 
+            {
+                MessageBox.Show("Proszę wypełnić wszystkie pola", "Błąd walidacji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            //Akceptacja EULA
+            //Pobieramy dane i zapisujemy do zmiennej
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
